d9e690a6d38046a8f5f5a1e37360947159780fb3
respectable2007 <zhanghui8876@163.com>
2019-10-08 15:40:25 +0800

    fragment

--- app/src/main/java/com/zh/example/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/zh/example/criminalintent/CrimeFragment.java
@@ -1,5 +1,67 @@
 package com.zh.example.criminalintent;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
-public class CrimeFragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import static android.widget.CompoundButton.OnCheckedChangeListener;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mCrimeTitle;
+    private Button mButtonDate;
+    private CheckBox mSolved;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        mCrimeTitle = (EditText)v.findViewById(R.id.crime_title);
+        mCrimeTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//              mCrimeTitle.setText(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mButtonDate = (Button)v.findViewById(R.id.crime_date);
+        mButtonDate.setText(mCrime.getDate().toString());
+        mButtonDate.setEnabled(false);
+        mSolved = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolved.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.d("renren", String.valueOf(b));
+                mCrime.setSolved(b);
+            }
+        });
+        return v;
+    }
 }
--- app/src/main/java/com/zh/example/criminalintent/MainActivity.java
+++ app/src/main/java/com/zh/example/criminalintent/MainActivity.java
@@ -1,8 +1,10 @@
 package com.zh.example.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
-//import android.support.v4.app.*
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
